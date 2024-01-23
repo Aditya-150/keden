829719b89df81a5c3d7e493f6fd97dcea555fbe3
Debidatta <74270500+debidattasuryaprakash@users.noreply.github.com>
2024-01-23 22:15:00 +0530

    Logos changed

--- src/components/LogoCarousel/index.js
+++ src/components/LogoCarousel/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Image from "next/image";
-import logo1 from "../../../public/images/instacart.png";
-import logo2 from "../../../public/images/toyota.png";
-import logo3 from "../../../public/images/cisco.png";
-import logo4 from "../../../public/images/microsoft.png";
-import logo5 from "../../../public/images/strava.png";
+import logo1 from "../../../public/images/logo/nz-fortinet.png";
+import logo2 from "../../../public/images/logo/nz-automox.png";
+import logo3 from "../../../public/images//logo/nz-aws.png";
+import logo4 from "../../../public/images/logo/nz-microsoft.png";
+import logo5 from "../../../public/images/logo/nz-shophos.png";
+import logo6 from "../../../public/images/logo/nz-vm-ware.png";
+import logo7 from "../../../public/images/logo/nz-kaseya.png";
+import logo8 from "../../../public/images//logo/nz-cisco.png";
+import logo9 from "../../../public/images/logo/nz-zerto.png";
+import logo10 from "../../../public/images/logo/nz-azure.png";
 import "./styles.css";
 
 export default function LogoCarousel() {
@@ -22,6 +27,11 @@ export default function LogoCarousel() {
           <Image src={logo3} alt="logo slide" />
           <Image src={logo4} alt="logo slide" />
           <Image src={logo5} alt="logo slide" />
+          <Image src={logo6} alt="logo slide" />
+          <Image src={logo7} alt="logo slide" />
+          <Image src={logo8} alt="logo slide" />
+          <Image src={logo9} alt="logo slide" />
+          <Image src={logo10} alt="logo slide" />
         </div>
         <div className="logos-slide">
           <Image src={logo1} alt="logo slide" />
@@ -29,6 +39,11 @@ export default function LogoCarousel() {
           <Image src={logo3} alt="logo slide" />
           <Image src={logo4} alt="logo slide" />
           <Image src={logo5} alt="logo slide" />
+          <Image src={logo6} alt="logo slide" />
+          <Image src={logo7} alt="logo slide" />
+          <Image src={logo8} alt="logo slide" />
+          <Image src={logo9} alt="logo slide" />
+          <Image src={logo10} alt="logo slide" />
         </div>
       </div>
     </section>
